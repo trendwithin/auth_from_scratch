a7d79ab4328225c2e5770a253ab187b40965a3fe
Michael Becco <e4e6d4d5@gmail.com>
2015-03-20 14:16:19 -0700

    Init Repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AuthFromScratch::Application.config.secret_token = 'b7e1bc30ae78b4164637ab7c0dbb83000dcd927983a73c266828f9cce388732c86e2b9c42a152546696ad16e6a3a540b417b62037502f46d31fb810e88c150c2'
